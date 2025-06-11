499a97cbe167082bd5f7a6caf75679fee1aab352
Ben Ronkin <ronkinben@gmail.com>
2025-06-11 16:12:28 -0700

    Refactored app

--- docs/notes/sections/mainPanel.js
+++ docs/notes/sections/mainPanel.js
@@ -43,7 +43,7 @@ const css = `
 export function mainPanel() {
   injectStyle(css)
 
-  const el = createDiv({ className: 'mt-20' })
+  const el = createDiv({ className: 'mt-20 hidden' })
 
   build(el)
   react(el)
