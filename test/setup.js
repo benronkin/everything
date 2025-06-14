b307b3b8231bf47f38086e8accae1547561df54f
Ben Ronkin <ronkinben@gmail.com>
2025-06-13 19:57:29 -0700

    Fix: shopping issues

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
 import { JSDOM } from 'jsdom'
 
-const dom = new JSDOM(`<!DOCTYPE html><body></body>`)
+const dom = new JSDOM(`<!DOCTYPE html><body class="dark-mode"></body>`)
 global.window = dom.window
 global.document = dom.window.document
 global.HTMLElement = dom.window.HTMLElement
