7edd97ff35fe8bb240bc9874014846c7d8e735b4
Ben Ronkin <ronkinben@gmail.com>
2025-06-15 10:00:27 -0700

    country-state-city

--- docs/assets/composites/countryStateCity.js
+++ docs/assets/composites/countryStateCity.js
@@ -1,15 +1,10 @@
 import { injectStyle } from '../js/ui.js'
 import { createDiv } from '../partials/div.js'
-import { createSpanGroup } from '../partials/spanGroup.js'
 import { createInputGroup } from '../partials/inputGroup.js'
 import { createIcon } from '../partials/icon.js'
 import { createHeader } from '../partials/header.js'
 import { state } from '../js/state.js'
 import { log } from '../js/logger.js'
-
-// -------------------------------
-// Globals
-// -------------------------------
 
 const css = `
 .country-state-city {
@@ -20,18 +15,15 @@ const css = `
   flex-direction: column;
 }
 .country-state-city .edit-header {
+  background: var(--gray1);
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-bottom: 2px solid var(--gray0);
-  padding: 10px 0;
-  margin-bottom: 30px;
+  margin-top: 10px;
+  height: 11px;
 }
 `
-
-// -------------------------------
-// Exports
-// -------------------------------
 
 export function createCountryStateCity({ id, className } = {}) {
   injectStyle(css)
@@ -44,99 +36,50 @@ export function createCountryStateCity({ id, className } = {}) {
 
   id && (el.id = id)
   className && (el.className = className)
-
-  el.querySelectorAll('.field').forEach((input) => (input.disabled = true))
-
   return el
 }
 
-// -------------------------------
-// Helpers
-// -------------------------------
-
 function build(el) {
-  viewPanel(el)
-  editPanel(el)
-}
-
-function react(el) {
-  state.on('country-state-city-page', 'countryStateCity', (page) => {
-    const pages = ['country', 'state', 'city']
-    pages.forEach((name, idx) => {
-      const group = el
-        .querySelector(`.edit-panel input[name="${name}"]`)
-        ?.closest('.input-group')
-      group.classList.toggle('hidden', idx !== page)
-    })
-
-    el.querySelector('.fa-chevron-left')?.classList.toggle('hidden', page === 0)
-    el.querySelector('.fa-chevron-right')?.classList.toggle(
-      'hidden',
-      page === 2
-    )
-    el.querySelector('.fa-close')?.classList.toggle('hidden', page !== 2)
-    el.querySelector('.edit-header h5').textContent = pages[page].toUpperCase()
-  })
-}
-
-function listen(el) {
-  el.querySelector('.view-panel').addEventListener('click', () => {
-    el.querySelector('.view-panel').classList.add('hidden')
-    el.querySelector('.edit-panel').classList.remove('hidden')
-  })
-
-  el.querySelector('.fa-chevron-left')?.addEventListener('click', () => {
-    const page = state.get('country-state-city-page')
-    state.set('country-state-city-page', page - 1)
-  })
-
-  el.querySelector('.fa-chevron-right')?.addEventListener('click', () => {
-    const page = state.get('country-state-city-page')
-    state.set('country-state-city-page', page + 1)
-  })
-
-  el.querySelector('.fa-close')?.addEventListener('click', () => {
-    el.querySelector('.view-panel').classList.remove('hidden')
-    el.querySelector('.edit-panel').classList.add('hidden')
-    state.set('country-state-city-page', 0)
-  })
-}
-
-function viewPanel(el) {
-  const viewPanel = createDiv({ className: 'view-panel' })
-  el.appendChild(viewPanel)
-
-  viewPanel.appendChild(
-    createInputGroup({
-      name: 'city',
-      placeholder: 'City',
-      classes: { group: 'mb-40', input: 'field', icon: 'fa-city' },
-    })
-  )
-
-  viewPanel.appendChild(
-    createInputGroup({
-      name: 'state',
-      placeholder: 'State',
-      classes: { group: 'mb-40', input: 'field', icon: 'fa-map' },
-    })
-  )
-
-  viewPanel.appendChild(
+  el.appendChild(
     createInputGroup({
       name: 'country',
       placeholder: 'Country',
-      classes: { input: 'field', icon: 'fa-flag' },
+      autocomplete: 'off',
+      classes: {
+        input: 'field',
+        icon: 'fa-flag',
+      },
     })
   )
-}
 
-function editPanel(el) {
-  const editPanel = createDiv({ className: 'edit-panel outer-wrapper hidden' })
-  el.appendChild(editPanel)
+  el.appendChild(
+    createInputGroup({
+      name: 'state',
+      autocomplete: 'off',
+      placeholder: 'State',
+      classes: {
+        input: 'field',
+        icon: 'fa-map',
+      },
+    })
+  )
+
+  el.appendChild(
+    createInputGroup({
+      name: 'city',
+      autocomplete: 'off',
+      placeholder: 'City',
+      classes: {
+        input: 'field',
+        icon: 'fa-city',
+      },
+    })
+  )
+
+  el.appendChild(createDiv({ className: 'autocomplete' }))
 
   const headerEl = createDiv({
-    className: 'edit-header',
+    className: 'edit-header hidden',
     html: [
       // div needed here for flex parent to keep header centered
       createDiv({
@@ -162,41 +105,85 @@ function editPanel(el) {
       }),
     ],
   })
-  editPanel.appendChild(headerEl)
+  el.appendChild(headerEl)
+}
 
-  const bodyEl = createDiv({ className: 'edit-body' })
-  editPanel.appendChild(bodyEl)
+function react(el) {
+  state.on('country-state-city-page', 'countryStateCity', (page) => {
+    const pages = ['', 'country', 'state', 'city']
+    for (let i = 0; i < 4; i++) {
+      if (i == 0) continue
+      const name = pages[i]
+      const group = el
+        .querySelector(`input[name="${name}"]`)
+        ?.closest('.input-group')
+      group.classList.toggle('hidden', i !== page && page > 0)
+    }
 
-  bodyEl.appendChild(
-    createInputGroup({
-      name: 'country',
-      placeholder: 'Country',
-      classes: {
-        input: 'field',
-        icon: 'fa-flag',
-      },
-    })
+    el.querySelector('.fa-chevron-left')?.classList.toggle('hidden', page < 2)
+    el.querySelector('.fa-chevron-right')?.classList.toggle(
+      'hidden',
+      page === 0 || page === 3
+    )
+    el.querySelector('.fa-close')?.classList.toggle('hidden', page !== 3)
+
+    el.querySelector('.edit-header h5').textContent = pages[page].toUpperCase()
+    el.querySelector('.edit-header').classList.toggle('hidden', page === 0)
+  })
+}
+
+function listen(el) {
+  /* navigate to the right page after a click */
+  el.addEventListener(
+    'click',
+    (e) => {
+      // ignore clicks while on edit pages 1-3
+      if (state.get('country-state-city-page') !== 0) return
+
+      let page = 0
+
+      if (!el.querySelector('input[name="country"]').value.trim().length) {
+        // country value is needed
+        page = 1
+      } else if (!el.querySelector('input[name="state"]').value.trim().length) {
+        // state value is needed
+        page = 2
+      } else {
+        // pick the page based on the element that was clicked on
+        // a non-input element will not have a name property
+        const pages = {
+          none: 1,
+          country: 1,
+          state: 2,
+          city: 3,
+        }
+        page = pages[e.target.name || 'none']
+      }
+      state.set('country-state-city-page', page)
+    },
+    true
   )
 
-  bodyEl.appendChild(
-    createInputGroup({
-      name: 'state',
-      placeholder: 'State',
-      classes: {
-        input: 'field',
-        icon: 'fa-map',
-      },
-    })
+  /* return to page 0 */
+  el.addEventListener(
+    'keydown',
+    (e) => {
+      if (e.key === 'Escape') state.set('country-state-city-page', 0)
+    },
+    true
   )
 
-  bodyEl.appendChild(
-    createInputGroup({
-      name: 'city',
-      placeholder: 'City',
-      classes: {
-        input: 'field',
-        icon: 'fa-city',
-      },
-    })
-  )
+  el.querySelector('.fa-chevron-left')?.addEventListener('click', () => {
+    const page = state.get('country-state-city-page')
+    state.set('country-state-city-page', page - 1)
+  })
+
+  el.querySelector('.fa-chevron-right')?.addEventListener('click', () => {
+    const page = state.get('country-state-city-page')
+    state.set('country-state-city-page', page + 1)
+  })
+
+  el.querySelector('.fa-close')?.addEventListener('click', () => {
+    state.set('country-state-city-page', 0)
+  })
 }
