1a1115bd4351c4ce773e1720675bfb692fd50cbd
Ben Ronkin <ronkinben@gmail.com>
2025-07-05 13:05:59 -0700

    Home page

--- docs/assets/composites/editor.js
+++ docs/assets/composites/editor.js
@@ -24,7 +24,9 @@ const css = `
 .editor-wrapper .viewer h1:not(:first-child) {
   margin-top: 30px;
 }
-.editor-wrapper .viewer h2 {
+.editor-wrapper .viewer h1,
+.editor-wrapper .viewer h2,
+.editor-wrapper .viewer h3 {
   margin: 20px 0 5px;
 }
 .editor-wrapper .viewer hr {
