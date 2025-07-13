845e1142650557e7cc3de59c3f81bf69425e78a8
Ben Ronkin <ronkinben@gmail.com>
2025-07-13 08:48:56 -0700

    404

--- docs/assets/composites/footer.js
+++ docs/assets/composites/footer.js
@@ -27,7 +27,7 @@ footer span {
 
 const html = `
 <div class="container">
-  <span>Version: </span><span id="version-number">5.2.14</span>
+  <span>Version: </span><span id="version-number">5.2.15</span>
 </div>
 `
 
