14ff60eb16f0f50fac74478385982d374b3dbcbd
Ben Ronkin <ronkinben@gmail.com>
2025-07-14 14:37:09 -0700

    Lexicon: show submitter in left-panel

--- docs/lexicon/sections/mainDocumentsList.js
+++ docs/lexicon/sections/mainDocumentsList.js
@@ -1,15 +1,10 @@
 import { state } from '../../assets/js/state.js'
 import { setMessage } from '../../assets/js/ui.js'
+import { createDiv } from '../../assets/partials/div.js'
+import { createSpan } from '../../assets/partials/span.js'
 import { createMainDocumentsList } from '../../assets/partials/mainDocumentsList.js'
 import { createMainDocumentItem } from '../../assets/partials/mainDocumentItem.js'
 
-// -------------------------------
-// Exports
-// -------------------------------
-
-/**
- * Constuctor of a custom element
- */
 export function mainDocumentsList() {
   const el = createMainDocumentsList({
     id: 'left-panel-list',
@@ -21,22 +16,26 @@ export function mainDocumentsList() {
   return el
 }
 
-// -------------------------------
-// Helpers
-// -------------------------------
-
-/**
- * Subscribe to state
- */
 function react(el) {
   state.on('main-documents', 'mainDocumentsList', (docs) => {
     // populate children
     const children = docs.map((doc) => {
-      const html = doc.entry
+      const html = createItemTitle(doc)
       return createMainDocumentItem({ id: doc.id, html })
     })
     el.deleteChildren().addChildren(children)
   })
 
   setMessage()
+}
+
+function createItemTitle(doc) {
+  const div = createDiv({
+    className: 'flex justify-between w-100',
+    html: [
+      createSpan({ html: doc.entry }),
+      createSpan({ html: doc.submitterName, className: 'c-gray3' }),
+    ],
+  })
+  return div
 }
