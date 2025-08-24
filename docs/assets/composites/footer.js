73b16462cb249e60c4343073f13ca8a069e16b79
Ben Ronkin <ronkinben@gmail.com>
2025-08-24 06:15:43 -0700

    transition duration

--- docs/assets/composites/footer.js
+++ docs/assets/composites/footer.js
@@ -27,7 +27,7 @@ footer span {
 
 const html = `
 <div class="container">
-  <span>Version: </span><span id="version-number">5.4.22</span>
+  <span>Version: </span><span id="version-number">5.4.23</span>
 </div>
 `
 
