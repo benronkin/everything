d5a5dd73d21444102c865c6bd355445eee48cc5e
Ben Ronkin <ronkinben@gmail.com>
2025-09-10 14:50:29 -0700

    Recipes view by category

--- docs/recipes/sections/mainDocumentsList.js
+++ docs/recipes/sections/mainDocumentsList.js
@@ -1,5 +1,7 @@
 import { state } from '../../assets/js/state.js'
 import { setMessage } from '../../assets/js/ui.js'
+import { createDiv } from '../../assets/partials/div.js'
+import { createSpan } from '../../assets/partials/span.js'
 import { createMainDocumentsList } from '../../assets/partials/mainDocumentsList.js'
 import { createMainDocumentItem } from '../../assets/partials/mainDocumentItem.js'
 
@@ -21,7 +23,17 @@ function react(el) {
   state.on('main-documents', 'mainDocumentsList', (docs) => {
     // populate children
     const children = docs.map((doc) => {
-      const html = doc.title
+      const htmlArr = [createSpan({ html: doc.title })]
+      if (doc.category)
+        htmlArr.push(
+          createSpan({ html: `(${doc.category})`, className: 'c-gray3' })
+        )
+
+      const html = createDiv({
+        className: 'flex justify-between w-100',
+        html: htmlArr,
+      })
+
       return createMainDocumentItem({ id: doc.id, html })
     })
     el.deleteChildren().addChildren(children)
