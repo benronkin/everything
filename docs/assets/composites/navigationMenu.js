36e8109e541512fbfa17c0b780f76272e511905d
Ben Ronkin <ronkinben@gmail.com>
2026-04-28 17:14:12 -0700

    Journal break

--- docs/assets/composites/navigationMenu.js
+++ docs/assets/composites/navigationMenu.js
@@ -1,8 +1,13 @@
 import { injectStyle, navList } from '../js/ui.js'
 import { createList } from '../partials/list.js'
 import { createMenuItem } from '../partials/menuItem.js'
+import { createHeader } from '../partials/header.js'
 
 const css = `
+.nav-category {
+  margin: 20px 20px 10px;
+  border-bottom: 1px solid var(--gray3);
+}
 `
 
 export function createNavigationMenu({ container, active } = {}) {
@@ -21,14 +26,30 @@ export function createNavigationMenu({ container, active } = {}) {
 }
 
 function build({ el, active }) {
-  const listItems = navList.map((ni) =>
-    createMenuItem({
-      html: `<i class="fa-solid ${ni.icon}"></i> ${ni.label}`,
-      url: `../${ni.url}`,
-      id: `rd-item-${ni.id}`,
-      className: 'menu-item',
-    }),
-  )
+  const listItems = []
+  const categories = []
+
+  for (const ni of navList) {
+    if (ni.category && !categories.includes(ni.category)) {
+      listItems.push(
+        createHeader({
+          type: 'h5',
+          html: ni.category,
+          className: 'nav-category',
+        }),
+      )
+      categories.push(ni.category)
+    }
+
+    listItems.push(
+      createMenuItem({
+        html: `<i class="fa-solid ${ni.icon}"></i> ${ni.label}`,
+        url: `../${ni.url}`,
+        id: `rd-item-${ni.id}`,
+        className: 'menu-item',
+      }),
+    )
+  }
 
   el.addChildren(listItems)
   if (active) {
