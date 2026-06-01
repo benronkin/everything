14d3cef0c7a14996313ee1057e71663e97b4b63e
Ben Ronkin <ronkinben@gmail.com>
2026-06-01 11:28:23 -0700

    Project list categories

--- docs/projects/sections/mainDocumentsList.js
+++ docs/projects/sections/mainDocumentsList.js
@@ -1,5 +1,6 @@
 import { injectStyle } from '../../assets/js/ui.js'
 import { state } from '../../assets/js/state.js'
+import { createDiv } from '../../assets/partials/div.js'
 import { createIcon } from '../../assets/partials/icon.js'
 import { createList } from '../../assets/partials/list.js'
 import { createSpan } from '../../assets/partials/span.js'
@@ -40,21 +41,56 @@ function react(el) {
       return
     }
 
-    const children = docs.map(({ id, title, starts_at, className }) => {
-      const obj = formatDateParts(starts_at)
-      const startDate = `${obj.month}/${obj.day}/${obj.shortYear}`
+    const dict = {
+      Active: [],
+      Archive: []
+    }
 
-      const html = [
-        createSpan({ html: title }),
-        createSpan({ html: `(${startDate})`, className: 'c-gray3 text-right' })
-      ]
-      return createMainDocumentLink({
-        id,
-        className: `md-item list-item ${className}`.trim(),
-        html,
-        url: `${state.getBaseUrl()}projects/project.html?id=${id}`
+    const today = new Date()
+
+    for (const doc of docs) {
+      if (!doc.ends_at || new Date(doc.ends_at) > today) {
+        dict.Active.push(doc)
+      } else {
+        dict.Archive.push(doc)
+      }
+    }
+
+    if (dict.Archive.length > 1) {
+      dict.Archive.sort((a, b) => {
+        const dateA = a.starts_at ? new Date(a.starts_at) : new Date(0)
+        const dateB = b.starts_at ? new Date(b.starts_at) : new Date(0)
+        return dateB - dateA
       })
-    })
+    }
+
+    const children = []
+
+    for (const [cat, docs] of Object.entries(dict)) {
+      if (docs.length) {
+        children.push(createDiv({ html: cat, className: 'category' }))
+        for (const { id, title, starts_at, className } of docs) {
+          const obj = formatDateParts(starts_at)
+          const startDate = `${obj.month}/${obj.day}/${obj.shortYear}`
+
+          const html = [
+            createSpan({ html: title }),
+            createSpan({
+              html: `(${startDate})`,
+              className: 'c-gray3 text-right'
+            })
+          ]
+          children.push(
+            createMainDocumentLink({
+              id,
+              className: `md-item list-item ${className}`.trim(),
+              html,
+              url: `${state.getBaseUrl()}projects/project.html?id=${id}`
+            })
+          )
+        }
+      }
+    }
 
     el.addChildren(children)
   })
